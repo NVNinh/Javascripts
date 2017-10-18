fe6c09b89d443fe6a5ffb4b887510d3a4d799d1e
NinhNguyen <sup>
2017-10-18 23:49:28 +0700

    learn object - countine

--- Object.js
+++ Object.js
@@ -36,3 +36,19 @@ var x8 = new Date();      // A new Date object
 
 var date = new Date('2015-03-25');
 console.log(date);
+//////////////////////////
+var flight = {status: "Flying"};
+
+//Object is passed by reference by default
+var byRefFlight = flight;
+
+//This code really copies object to other instance
+var byCopyFlight = Object.create(flight);
+
+byRefFlight.status = "Landing";
+console.log('flight.status = ' + flight.status + ". byRefFlight.status = " + byRefFlight.status);
+//flight.status = Landing. byRefFlight.status = Landing
+
+byCopyFlight.status = "Crash!";
+console.log('flight.status = ' + flight.status + ". byCopyFlight.status = " + byCopyFlight.status);
+//flight.status = Landing. byCopyFlight.status = Crash!
